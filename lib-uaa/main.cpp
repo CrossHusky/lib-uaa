3ebb92058edc15ca78b043fdef162eb696c6e0e6
Samael Martínez <samael@pop-os.localdomain>
2020-06-14 22:33:41 -0500

    Añadir manejo de archivo binario-texto

--- lib-uaa/main.cpp
+++ lib-uaa/main.cpp
@@ -6,53 +6,78 @@
 using namespace std;
 
 // Estructura para el manejo del archivo
-struct persona {
+struct alumno
+{
     char pNom[100];
     char pDir[100];
     char pTel[100];
+    char pEdad[3];
 };
 
 int main(void)
 {
 
     // Datos a usar
-    FILE* fp1;
-    persona p;
+    FILE *fBin, *fText;
+    alumno p;
 
     // Apertura del archivo
-    fp1 = fopen("File.txt", "r+");
-    if(fp1 == NULL) {
-        fp1 = fopen("File.txt", "w+");
-        if(fp1 == NULL) {
-            printf("Error al abrir archivo. \n");
-            return -1;
-        }
+    fBin = fopen("File.bin", "ab+");
+    fText = fopen("File.txt", "w+");
+
+    if (fBin == NULL)
+    {
+        printf("No se puede leer el archivo...");
+        return -1;
+    }
+
+    if (fText == NULL)
+    {
+        printf("No se puede leer el archivo...");
+        return -1;
     }
 
     // Escritura en el archivo
-    fseek(fp1, 0, SEEK_END);
-    printf("Nombre:");
-    scanf("%[^\n]", p.pNom);
-    cin.ignore();
-    printf("Dirección:");
-    scanf("%[^\n]", p.pDir);
-    cin.ignore();
-    printf("Teléfono:");
-    scanf("%[^\n]", p.pTel);
-    cin.ignore();
-    fwrite(&p, sizeof(p), 1, fp1);
-    
-    //Lectura del archivo
-    fread(&p, sizeof(p), 1, fp1);
-    printf("Nombre:");
-    printf("%s \n", p.pNom);
-    printf("Dirección:");
-    printf("%s \n", p.pDir);
-    printf("Teléfono:");
-    printf("%s \n", p.pTel);
-    
-    //Cerrar archivo
-    fclose(fp1);
-    
+    for (int i = 0; i < 5; i++)
+    {
+        fseek(fBin, 0, SEEK_END);
+        printf("Nombre:");
+        scanf("%[^\n]", p.pNom);
+        cin.ignore();
+        printf("Dirección:");
+        scanf("%[^\n]", p.pDir);
+        cin.ignore();
+        printf("Teléfono:");
+        scanf("%[^\n]", p.pTel);
+        cin.ignore();
+        printf("Edad:");
+        scanf("%[^\n]", p.pEdad);
+        cin.ignore();
+        fwrite(&p, sizeof(struct alumno), 1, fBin);
+    }
+
+    // Lectura del archivo
+    for (int i = 0; i < 5; i++)
+    {
+        fseek(fBin, i * sizeof(struct alumno), SEEK_SET);
+        fread(&p, sizeof(struct alumno), 1, fBin);
+        printf("\nDatos del alumno %d:\n", i + 1);
+        printf("Nombre:");
+        printf("%s \n", p.pNom);
+        printf("Dirección:");
+        printf("%s \n", p.pDir);
+        printf("Teléfono:");
+        printf("%s \n", p.pTel);
+        printf("Edad:");
+        printf("%s \n", p.pEdad);
+        // Copia al archivo de Texto
+        fseek(fText, 0, SEEK_END);
+        fwrite(&p, sizeof(struct alumno), 1, fText);
+    }
+
+    // Cerrar archivo
+    fclose(fBin);
+    fclose(fText);
+
     return 0;
 }
